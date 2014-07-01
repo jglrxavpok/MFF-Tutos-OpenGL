a52ae5f8dceec71e11dd087dd68b050bee0d4484
jglrxavpok <jglrxavpok@gmail.com>
2014-07-01 15:25:28 +0200

    Préparation de la boucle de rendu!

--- fr/mff/tutos/ogl/OpenGLTuto1.java
+++ fr/mff/tutos/ogl/OpenGLTuto1.java
@@ -16,11 +16,19 @@ public class OpenGLTuto1
 			GL11.glLoadIdentity();
 			GL11.glOrtho(0, 800, 0, 600, 1, -1);
 			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			while(!Display.isCloseRequested())
+				tick();
 		}
 		catch(LWJGLException e)
 		{
 			e.printStackTrace();
 		}
+	}
+
+	private static void tick()
+	{
+		Display.sync(60);
+		Display.update();
 	}
 
 }
