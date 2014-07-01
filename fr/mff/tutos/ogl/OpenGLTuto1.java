ccbf320939b16bffb3c42605363f9898f9f88d69
jglrxavpok <jglrxavpok@gmail.com>
2014-07-01 14:54:22 +0200

    On teste que tout marche

--- fr/mff/tutos/ogl/OpenGLTuto1.java
+++ fr/mff/tutos/ogl/OpenGLTuto1.java
@@ -1,11 +1,21 @@
 package fr.mff.tutos.ogl;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
 public class OpenGLTuto1
 {
 
 	public static void main(String[] args)
 	{
-		
+		try
+		{
+			Display.create();
+		}
+		catch(LWJGLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
