c23f035979a16ae8259f9eaf8a7f13663ce884b4
jglrxavpok <jglrxavpok@gmail.com>
2014-07-02 15:47:19 +0200

    Texture !

--- fr/mff/tutos/ogl/OpenGLTuto1.java
+++ fr/mff/tutos/ogl/OpenGLTuto1.java
@@ -2,14 +2,25 @@ package fr.mff.tutos.ogl;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 public class OpenGLTuto1
 {
 
 	private static int angle;
+	private static int textureID;
 
 	public static void main(String[] args)
 	{
@@ -20,6 +31,10 @@ public class OpenGLTuto1
 			GL11.glLoadIdentity();
 			GL11.glOrtho(0, 800, 0, 600, 1, -1);
 			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			glEnable (GL_BLEND);
+			glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glEnable(GL_TEXTURE_2D);
+			loadTexture();
 			while(!Display.isCloseRequested())
 				tick();
 		}
@@ -28,26 +43,81 @@ public class OpenGLTuto1
 			e.printStackTrace();
 		}
 	}
+	
+	private static void loadTexture()
+	{
+		textureID = glGenTextures();
+		try
+		{
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			BufferedImage img = ImageIO.read(OpenGLTuto1.class.getResourceAsStream("/OpenGL_Logo.png"));
+			int[] pixels = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+			FloatBuffer pixelsBuffer = BufferUtils.createByteBuffer(pixels.length * 4 * 4).asFloatBuffer();
+			for(int y = img.getHeight()-1;y>=0;y--)
+			{
+				for(int x = 0;x<img.getWidth();x++)
+				{
+					int pixel = pixels[y*img.getWidth()+x];
+					float a = ((pixel >> 24) & 0xFF)/255f;
+					float r = ((pixel >> 16) & 0xFF)/255f;
+					float g = ((pixel >> 8) & 0xFF)/255f;
+					float b = ((pixel >> 0) & 0xFF)/255f;
+    				
+					if(a == 0.f)
+					{
+						pixelsBuffer.put(0);
+    					pixelsBuffer.put(0);
+    					pixelsBuffer.put(0);
+    					pixelsBuffer.put(0);
+					}
+					else
+					{
+    					pixelsBuffer.put(r);
+    					pixelsBuffer.put(g);
+    					pixelsBuffer.put(b);
+    					pixelsBuffer.put(a);
+					}
+				}
+			}
+			
+			pixelsBuffer.flip();
+			
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+			GL11.glTexEnvf(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
+			
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.getWidth(), img.getHeight(), 0, GL_RGBA, GL_FLOAT, pixelsBuffer);
+			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
 	private static void tick()
 	{
+		glClearColor(1, 0,0, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glBindTexture(GL_TEXTURE_2D, textureID);
 		glPushMatrix();
-		glScaled(2, 2, 2);
-		glTranslated(50, 0, 0);
+		glTranslated(50, 50, 0);
+		glBegin(GL_QUADS);
 		
-		glTranslated(50, 150, 0);
-		glRotated(angle++, 0, 0, 1);
-		glTranslated(-50, -150, 0);
-
-		
-		glBegin(GL_TRIANGLES);
-			glColor3f(0, 0, 1);
-			glVertex2d(0, 100);
-			glColor3f(0, 1, 0);
-			glVertex2d(50, 100+100);
-			glColor3f(1, 0, 0);
-			glVertex2d(100, 100);
+			glTexCoord2d(0, 0);
+			glVertex2d(0, 0);
+			
+			glTexCoord2d(0, 1);
+			glVertex2d(0, 149);
+			
+			glTexCoord2d(1, 1);
+			glVertex2d(300, 149);
+			
+			glTexCoord2d(1, 0);
+			glVertex2d(300, 0);
+			
 		glEnd();
 		glPopMatrix();
 		Display.update();
